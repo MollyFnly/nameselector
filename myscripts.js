397bd800420d6fec2b57b2afbc8daef86e721c06
Finley <cmfinley3@gmail.com>
2022-02-20 00:58:53 -0500

    Basic Functionality

---
+++ myscripts.js
@@ -0,0 +1,13 @@
+let names = ["Gwen", "Vi", "Saint", "The", "Lavendar", "Tost", "Legion", "Aofie", "Anura"];
+
+function buttonAction() {
+    let button = document.getElementById("button");
+    let old_name = button.innerText;
+    let index = Math.floor(Math.random() * names.length);
+    let new_name = names[index];
+    if(new_name == old_name) {
+        new_name = names[(index + 1 ) % names.length]
+    }
+    button.innerText = new_name;
+}
+document.getElementById("button").addEventListener("click", buttonAction);
